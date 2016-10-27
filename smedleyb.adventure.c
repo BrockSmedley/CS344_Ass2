5047174173496336d7e28ee6753ce1dec9230d2e
Brock Smedley <brocksmedley@gmail.com>
2016-10-26 19:09:46 -0700

    Room files populate with everything besides connections

--- smedleyb.adventure.c
+++ smedleyb.adventure.c
@@ -6,7 +6,7 @@
 
 int true = 1;
 int false = 0;
-const int SIZE = 3;
+const int SIZE = 5;
 
 
 // accepts an array of integers representing taken name indices
@@ -37,8 +37,52 @@ int getRandomNameIndex(int* takenIndices){
   return r;
 }
 
+// create data file
+// format:
+// <roomNum>&<roomName>&<type>&<conn1>,<conn2>,<conn3>
+char* assembleData(int roomNum, char* roomName, char* roomType, char* connections[]){
+  char* data = malloc(420);
+  //char buffer[420];
+  
+  // convert roomNum to string
+  char rn[15];
+  sprintf(rn, "%d", roomNum); 
+ 
+  // add roomNum to data
+  strcpy(data, rn);
+  
+  // add roomName to data
+  strcat(data, "&");
+  strcat(data, roomName);
+
+  // add type to data
+  strcat(data, "&");
+  strcat(data, roomType);
+  
+  return data;
+}
+
+void populateRoomFileData(int roomNum, char* roomName, char* roomType, char* connections[], int file_descriptor){
+  char* data = assembleData(roomNum, roomName, roomType, connections);// "FUCKIN SHIT BRO THIS IS AN EXAMPLE";
+  int size = 512;// = sizeof data / sizeof *data;
+  char readBuffer[size];
+  
+  ssize_t nread, nwritten;
+
+  // write the file shit
+  nwritten = write(file_descriptor, data, strlen(data) * sizeof(char));
+
+  // read the file shit
+  memset(readBuffer, '\0', size);
+  lseek(file_descriptor, 0, SEEK_SET);
+  nread = read(file_descriptor, readBuffer, size);
+  printf("File contents: \n%s\n", readBuffer);
+
+  printf("nwritten: %d\n", nwritten);
+}
+
 // creates a temporary room file and returns its name
-char* createRoomFile(int roomNum, char* roomName, char* connections[]){
+char* createRoomFile(int roomNum, char* roomName, char* roomType, char* connections[]){
   char* fileName;
   int numConnections;
  
@@ -49,13 +93,16 @@ char* createRoomFile(int roomNum, char* roomName, char* connections[]){
   for (; i < numConnections; i++){
     // create temp file
     int file_descriptor;
-    file_descriptor = open(fileName, O_WRONLY | O_CREAT);
+    file_descriptor = open(fileName, O_RDWR | O_CREAT | O_TRUNC);
 
     if (file_descriptor < 0)
-      printf("Could not open %s\n", fileName);
-    else
-      printf("created file %s\n", fileName);
+      printf("Could not open file \"%s\" \n", fileName);
+    /*else
+      printf("O_CREAT created file \"%s\" \n", fileName);
+      */
+    
     // populate file with info
+    populateRoomFileData(roomNum, roomName, roomType, connections, file_descriptor);
   }
 
   return fileName;
@@ -111,7 +158,7 @@ int main(){
 
     printf("roomName: %s\n", roomName);
 
-    createRoomFile(i, roomName, NULL);
+    createRoomFile(i, roomName, roomType, NULL);
   }
 
   printf("takenRoomNames:\n");
